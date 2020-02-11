2e14b35dc8c32840516605256725d9a7f22709ba
ilakiya99 <55476356+ilakiya99@users.noreply.github.com>
2020-02-11 11:56:52 +0530

    Add files via upload

---
+++ calci.py
@@ -0,0 +1,10 @@
+num1 = int(input("number 1: "))
+num2 = int(input("number 2: "))
+sum = num1 + num2
+sub = num1 - num2
+mul = num1 * num2
+div = num1 / num2
+print("Addition = ",sum)
+print("Subtraction = ",sub)
+print("Multiplication = ",mul)
+print("Division = ",div)
